3a0a4fe3125bf4ec9b650848ec6d02f65d964a94
farthen <monkey@farthen.de>
2014-08-01 04:50:19 +0200

    Fix nicksub, update version

--- wurstminebot/nicksub.py
+++ wurstminebot/nicksub.py
@@ -5,6 +5,7 @@ import re
 
 CONFIG_FILE = '/opt/wurstmineberg/config/people.json'
 
+
 def config(person_id=None):
     try:
         with open(CONFIG_FILE) as config_file:
@@ -19,13 +20,17 @@ def config(person_id=None):
         if person.get('id') == person_id:
             return person
     else:
-        raise PersonNotFoundError('person with id ' + str(person_id) + ' not found')
+        raise PersonNotFoundError(
+            'person with id ' + str(person_id) + ' not found')
+
 
 def set_config(config_dict):
     if not isinstance(config_dict, dict):
         config_dict = {'people': config_dict}
     with open(CONFIG_FILE, 'w') as config_file:
-        json.dump(config_dict, config_file, sort_keys=True, indent=4, separators=(',', ': '))
+        json.dump(config_dict, config_file,
+                  sort_keys=True, indent=4, separators=(',', ': '))
+
 
 def update_config(person_id, path, value=None, delete=False):
     config_dict = config()
@@ -58,8 +63,12 @@ def update_config(person_id, path, value=None, delete=False):
             full_config_dict['people'][person_index] = value
     set_config(full_config_dict)
 
+
 class PersonNotFoundError(Exception):
-    pass # raised when a Person object is created or reloaded with data not in the config
+    # raised when a Person object is created or reloaded with data not in the
+    # config
+    pass
+
 
 def ircNicks(mode='all', include_ids=False):
     for person in config():
@@ -74,10 +83,12 @@ def ircNicks(mode='all', include_ids=False):
             else:
                 raise ValueError('unknown mode: ' + str(mode))
 
+
 def minecraftNicks(include_ids=False):
     for person in config():
         if 'minecraft' in person:
             yield (person['id'], person['minecraft']) if include_ids else person['minecraft']
+
 
 def otherNicks(mode='all', include_ids=False):
     for person in config():
@@ -89,6 +100,7 @@ def otherNicks(mode='all', include_ids=False):
                 yield (person['id'], person['nicks']) if include_ids else person['nicks']
             else:
                 raise ValueError('unknown mode: ' + str(mode))
+
 
 def redditNicks(include_ids=False, format='plain'):
     def _formatRedditNick(nick):
@@ -102,38 +114,42 @@ def redditNicks(include_ids=False, format='plain'):
             return 'https://reddit.com/u/' + nick
         else:
             raise ValueError('unknown format: ' + str(format))
-    
+
     for person in config():
         if 'reddit' in person:
             yield (person['id'], _formatRedditNick(person['reddit'])) if include_ids else _formatRedditNick(person['reddit'])
 
+
 def twitterNicks(include_ids=False, twitter_at_prefix=False):
     for person in config():
         if 'twitter' in person:
-            formatted_nick = ('@' + person['twitter']) if twitter_at_prefix else person['twitter']
+            formatted_nick = (
+                '@' + person['twitter']) if twitter_at_prefix else person['twitter']
             yield (person['id'], formatted_nick) if include_ids else formatted_nick
 
+
 class BasePerson:
+
     def __eq__(self, other):
         try:
             return self.id == other.id
         except AttributeError:
             return False
-    
+
     def del_option(self, option_name):
         opts = self.options
         try:
             del opts[option_name.lower()]
         except KeyError:
-            pass # no option to delete
+            pass  # no option to delete
         self.options = opts
-    
+
     def display_name(self):
         return self.id if self.name is None else self.name
-    
+
     def invited(self):
         return self.whitelisted() or self.status == 'invited'
-    
+
     def irc_nick(self, respect_highlight_option=True, channel_members=[], fallback=True):
         """Returns the best IRC nick for the person. “Best” in this case means the first in the list.
         
@@ -156,7 +172,7 @@ class BasePerson:
         if respect_highlight_option and not self.option('chatsync_highlight'):
             return nick[0] + '\u200c' + nick[1:]
         return nick
-    
+
     def nick(self, context, default=True, twitter_at_prefix=False):
         if default is True:
             default = self.display_name()
@@ -170,26 +186,30 @@ class BasePerson:
             return default if self.twitter is None else (('@' + self.twitter) if twitter_at_prefix else self.twitter)
         else:
             return default
-    
+
     def option(self, option_name):
-        default_true_options = ['chatsync_highlight', 'inactivity_tweets'] # These options are on by default. All other options are off by default.
+        # These options are on by default. All other options are off by
+        # default.
+        default_true_options = ['chatsync_highlight', 'inactivity_tweets']
         if option_name.lower() in self.options:
             return self.options[option_name.lower()]
         else:
             return option_name.lower() in default_true_options
-    
+
     def option_is_default(self, option_name):
         return option_name.lower() not in self.options
-    
+
     def set_option(self, option_name, value):
         opts = self.options
         opts[option_name.lower()] = value
         self.options = opts
-    
+
     def whitelisted(self):
         return self.status in ['founding', 'later', 'postfreeze']
 
+
 class Person(BasePerson):
+
     def __init__(self, id_or_nick, context=None, strict=False):
         if id_or_nick is None:
             raise TypeError('id or nick may not be None')
@@ -197,14 +217,15 @@ class Person(BasePerson):
             self.id = id_or_nick
             if not strict:
                 self.id = self.id.lower()
-            config(self.id) # raises PersonNotFoundError if the id is invalid
+            config(self.id)  # raises PersonNotFoundError if the id is invalid
         elif context == 'irc':
             for id, irc_nick in ircNicks(mode='all', include_ids=True):
                 if irc_nick.lower() == id_or_nick.lower():
                     self.id = id
                     break
             else:
-                raise PersonNotFoundError('person with IRC nick ' + str(id_or_nick) + ' not found')
+                raise PersonNotFoundError(
+                    'person with IRC nick ' + str(id_or_nick) + ' not found')
         elif context == 'minecraft':
             for id, minecraft_nick in minecraftNicks(include_ids=True):
                 if minecraft_nick.lower() == id_or_nick.lower():
@@ -220,7 +241,8 @@ class Person(BasePerson):
                     self.id = id
                     break
             else:
-                raise PersonNotFoundError('person with reddit nick ' + str(id_or_nick) + ' not found')
+                raise PersonNotFoundError(
+                    'person with reddit nick ' + str(id_or_nick) + ' not found')
         elif context == 'twitter':
             for id, twitter_nick in twitterNicks(include_ids=True, twitter_at_prefix=False):
                 if id_or_nick.startswith('@'):
@@ -229,22 +251,23 @@ class Person(BasePerson):
                     self.id = id
                     break
             else:
-                raise PersonNotFoundError('person with twitter nick ' + str(id_or_nick) + ' not found')
+                raise PersonNotFoundError(
+                    'person with twitter nick ' + str(id_or_nick) + ' not found')
         else:
             raise ValueError('unknown context: ' + str(context))
-    
+
     @property
     def description(self):
         return config(self.id).get('description')
-    
+
     @description.setter
     def description(self, value):
         update_config(self.id, ['description'], value=value)
-    
+
     @description.deleter
     def description(self):
         update_config(self.id, ['description'], delete=True)
-    
+
     @property
     def fav_color(self):
         color = config(self.id).get('favColor')
@@ -252,7 +275,7 @@ class Person(BasePerson):
             return color.get('red', 0), color.get('green', 0), color.get('blue', 0)
         else:
             return None
-    
+
     @fav_color.setter
     def fav_color(self, value):
         update_config(self.id, ['favColor'], value={
@@ -260,162 +283,164 @@ class Person(BasePerson):
             'green': value[1],
             'blue': value[2]
         })
-    
+
     @fav_color.deleter
     def fav_color(self):
         update_config(self.id, ['favColor'], value=None, delete=False)
-    
+
     @property
     def gravatar_email(self):
         return config(self.id).get('gravatar')
-    
+
     @gravatar_email.setter
     def gravatar_email(self, value):
         update_config(self.id, ['gravatar'], value=value)
-    
+
     @gravatar_email.deleter
     def gravatar_email(self):
         update_config(self.id, ['gravatar'], delete=True)
-    
+
     @property
     def irc_nicks(self):
         return config(self.id).get('irc', {}).get('nicks', [])
-    
+
     @irc_nicks.setter
     def irc_nicks(self, value):
         update_config(self.id, ['irc', 'nicks'], value=value)
-    
+
     @irc_nicks.deleter
     def irc_nicks(self):
         update_config(self.id, ['irc', 'nicks'], delete=True)
-    
+
     @property
     def minecraft(self):
         return config(self.id).get('minecraft')
-    
+
     @minecraft.setter
     def minecraft(self, value):
         update_config(self.id, ['minecraft'], value=value)
-    
+
     @minecraft.deleter
     def minecraft(self):
         update_config(self.id, ['minecraft'], delete=True)
-    
+
     @property
     def name(self):
         return config(self.id).get('name')
-    
+
     @name.setter
     def name(self, value):
         if value == self.id:
             update_config(self.id, ['name'], delete=True)
         else:
             update_config(self.id, ['name'], value=value)
-    
+
     @name.deleter
     def name(self):
         update_config(self.id, ['name'], delete=True)
-    
+
     @property
     def nicks(self):
         return config(self.id).get('nicks', [])
-    
+
     @nicks.setter
     def nicks(self, value):
         update_config(self.id, ['nicks'], value=value)
-    
+
     @nicks.deleter
     def nicks(self):
         update_config(self.id, ['nicks'], delete=True)
-    
+
     @property
     def nickserv(self):
         return config(self.id).get('irc', {}).get('nickserv')
-    
+
     @nickserv.setter
     def nickserv(self, value):
         update_config(self.id, ['irc', 'nickserv'], value=value)
-    
+
     @nickserv.deleter
     def nickserv(self):
         update_config(self.id, ['irc', 'nickserv'], delete=True)
-    
+
     @property
     def options(self):
         return config(self.id).get('options', {})
-    
+
     @options.setter
     def options(self, value):
         update_config(self.id, ['options'], value=value)
-    
+
     @options.deleter
     def options(self):
         update_config(self.id, ['options'], delete=True)
-    
+
     @property
     def reddit(self):
         return config(self.id).get('reddit')
-    
+
     @reddit.setter
     def reddit(self, value):
         update_config(self.id, ['reddit'], value=value)
-    
+
     @reddit.deleter
     def reddit(self):
         update_config(self.id, ['reddit'], delete=True)
-    
+
     @property
     def status(self):
         return config(self.id).get('status', 'later')
-    
+
     @status.setter
     def status(self, value):
         if value == 'later':
             del self.status
         else:
             update_config(self.id, ['status'], value=value)
-    
+
     @status.deleter
     def status(self):
         update_config(self.id, ['status'], delete=True)
-    
+
     @property
     def twitter(self):
         return config(self.id).get('twitter')
-    
+
     @twitter.setter
     def twitter(self, value):
         update_config(self.id, ['twitter'], value=value)
-    
+
     @twitter.deleter
     def twitter(self):
         update_config(self.id, ['twitter'], delete=True)
-    
+
     @property
     def website(self):
         return config(self.id).get('website')
-    
+
     @website.setter
     def website(self, value):
         update_config(self.id, ['website'], value=value)
-    
+
     @website.deleter
     def website(self):
         update_config(self.id, ['website'], delete=True)
-    
+
     @property
     def wiki(self):
         return config(self.id).get('wiki')
-    
+
     @wiki.setter
     def wiki(self, value):
         update_config(self.id, ['wiki'], value=value)
-    
+
     @wiki.deleter
     def wiki(self):
         update_config(self.id, ['wiki'], delete=True)
 
+
 class Dummy(BasePerson):
+
     def __init__(self, id_or_nick, context=None):
         if id_or_nick is None:
             raise TypeError('id or nick may not be None')
@@ -446,17 +471,19 @@ class Dummy(BasePerson):
             if id_or_nick.startswith('@'):
                 id_or_nick = id_or_nick[len('@'):]
             self.twitter = id_or_nick
-    
+
     def display_name(self):
         return self.id_or_nick
-    
+
     def nick(self, context, default=True, twitter_at_prefix=False):
         return super().nick(context, default=default, twitter_at_prefix=False)
+
 
 def everyone():
     for person in config():
         if 'id' in person:
             yield Person(person['id'])
+
 
 def exists(person_id):
     """Return True if a person with the given Wurstmineberg ID exists."""
@@ -466,6 +493,7 @@ def exists(person_id):
         return False
     else:
         return True
+
 
 def index(person, default=False):
     if isinstance(person, Person):
@@ -479,13 +507,16 @@ def index(person, default=False):
     for i, person_dict in enumerate(config()):
         if person_dict.get('id') == person_id:
             return i
-    raise PersonNotFoundError('person with id ' + str(person_id) + ' not found')
+    raise PersonNotFoundError(
+        'person with id ' + str(person_id) + ' not found')
+
 
 def person_or_dummy(id_or_nick, context=None):
     try:
         return Person(id_or_nick, context=context)
     except PersonNotFoundError:
         return Dummy(id_or_nick, context=context)
+
 
 def sorted_people(*args, context=None):
     """Returns a list of Person and Dummy objects based on the Person/Dummy objects and ids or (if context is specified) nicknames provided.
@@ -496,7 +527,7 @@ def sorted_people(*args, context=None):
         try:
             (_ for _ in args[0])
         except:
-            pass # argument is not iterable
+            pass  # argument is not iterable
         else:
             args = args[0]
     for person_or_id in args:
@@ -508,12 +539,14 @@ def sorted_people(*args, context=None):
             dummies.append(person_or_id)
     return sorted(people, key=index) + sorted(dummies, key=(lambda d: d.id_or_nick))
 
+
 def sub(nick, source, target, strict=True, exit_on_fail=False, twitter_at_prefix=True):
     if exit_on_fail:
         if nick is None:
             sys.exit(1)
         try:
-            ret = Person(nick, source).nick(target, twitter_at_prefix=twitter_at_prefix)
+            ret = Person(nick, source).nick(
+                target, twitter_at_prefix=twitter_at_prefix)
             if ret is None:
                 sys.exit(2)
             else:
@@ -528,6 +561,7 @@ def sub(nick, source, target, strict=True, exit_on_fail=False, twitter_at_prefix
         except PersonNotFoundError:
             return nick
 
+
 def textsub(text, source, target, strict=False):
     def _nicksForContext(context):
         if context == 'irc':
@@ -541,13 +575,16 @@ def textsub(text, source, target, strict=False):
             return twitterNicks(include_ids=True, twitter_at_prefix=True)
         else:
             return []
-    
+
+    urlcharacters = "[0-9A-Za-z-._~:/?#\[\]@!$&\'()*+\,;=]"
     text = text[:]
     for id, nick in _nicksForContext(source):
         if Person(id).nick(target):
-            text = re.sub('(?<![0-9A-Za-z@])(' + re.sub('\\|', '\\|', nick) + ')(?![0-9A-Za-z])', Person(id).nick(target, twitter_at_prefix=True), text, flags=re.IGNORECASE)
+            text = re.sub('(?<!' + urlcharacters + ')(' + re.sub('\\|', '\\|', nick) + ')(?!' + urlcharacters + ')', Person(
+                id).nick(target, twitter_at_prefix=True), text, flags=re.IGNORECASE)
     if not strict:
         for id, nick in otherNicks(include_ids=True, mode='all'):
             if Person(id).nick(target):
-                text = re.sub('(?<![0-9A-Za-z@])(' + re.sub('\\|', '\\|', nick) + ')(?![0-9A-Za-z])', Person(id).nick(target, twitter_at_prefix=True), text, flags=re.IGNORECASE)
+                text = re.sub('(?<!' + urlcharacters + ')(' + re.sub('\\|', '\\|', nick) + ')(?!' + urlcharacters + ')', Person(
+                    id).nick(target, twitter_at_prefix=True), text, flags=re.IGNORECASE)
     return text
