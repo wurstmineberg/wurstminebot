f6c012bf2333a50b432ade3905b4bef07d405fca
Fenhl <fenhl@fenhl.net>
2014-11-08 22:38:15 +0000

    Fix loops import

--- wurstminebot/loops.py
+++ wurstminebot/loops.py
@@ -6,8 +6,8 @@ from datetime import datetime
 from wurstminebot import deaths
 import json
 import lazyjson
-import loops
 import minecraft
+import loops as module_loops
 from wurstminebot import nicksub
 import os.path
 import random
@@ -19,7 +19,7 @@ from datetime import timedelta
 from datetime import timezone
 import traceback
 
-class InputLoop(loops.Loop):
+class InputLoop(module_loops.Loop):
     def iterable(self):
         timeout = 0.5
         error_timeout = 10
@@ -412,7 +412,7 @@ class InputLoop(loops.Loop):
             if core.state.get('is_daemon', False) or core.config('debug', False):
                 traceback.print_exc(file=sys.stdout)
 
-class TimeLoop(loops.Loop):
+class TimeLoop(module_loops.Loop):
     def iterable(self):
         #FROM http://stackoverflow.com/questions/9918972/running-a-line-in-1-hour-intervals-in-python
         # modified to work with leap seconds
@@ -438,7 +438,7 @@ class TimeLoop(loops.Loop):
     def process_value(value):
         tell_time(comment=True, restart=core.config('dailyRestart', True))
 
-class TwitterStream(loops.Loop):
+class TwitterStream(module_loops.Loop):
     def __init__(self, twitter_api):
         super().__init__(iterable=self.iterable(twitter_api))
     
